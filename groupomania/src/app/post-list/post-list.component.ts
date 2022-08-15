fa96e5f0b14b2a87dfec62b52fe00c1418e0cca1
dimitraye <dimitraye94@gmail.com>
2022-08-15 23:06:54 +0200

    mise en place de la liste des posts

--- groupomania/src/app/post-list/post-list.component.ts
+++ groupomania/src/app/post-list/post-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../services/posts.service';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { Post } from '../models/Post.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+  posts$!: Observable<Post[]>;
+  loading!: boolean;
+  errorMsg!: string;
 
-  ngOnInit(): void {
+  constructor(private post: PostsService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.loading = true;
+    this.posts$ = this.post.posts$.pipe(
+      tap(() => {
+        this.loading = false;
+        this.errorMsg = '';
+      }),
+      catchError(error => {
+        this.errorMsg = JSON.stringify(error);
+        this.loading = false;
+        return of([]);
+      })
+    );
+    this.post.getPosts();
+  }
+
+  onClickPost(id: string) {
+    this.router.navigate(['post', id]);
   }
 
 }
